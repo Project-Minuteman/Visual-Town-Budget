8fbf029bdfa9e71d7864d7a0a4df01a55a926f9a
Andrei Radulescu-Banu <44415560+andrei-radulescu-banu@users.noreply.github.com>
2023-03-11 22:33:05 -0500

    Devel

--- src/httpdocs/data/processing/processAll.sh
+++ src/httpdocs/data/processing/processAll.sh
@@ -2,9 +2,9 @@
 
 cp ../{revenues,expenses,funds,glossary}.csv .
 
-./processCSV.py revenues.csv
-./processCSV.py expenses.csv
-./processCSV.py funds.csv
+./processCSV.py ../revenues.csv
+./processCSV.py ../expenses.csv
+./processCSV.py ../funds.csv
 ./processCSV.py updatehome
-cp revenues.{csv,json} expenses.{csv,json} funds.{csv,json} glossary.csv home.json ..
+mv revenues.json expenses.json funds.json glossary.csv home.json ..
 
